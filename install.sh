2a60665eaaf2b441037779f1b96cbb2062f5b3ce
Kripth <markwhite1404@gmail.com>
2017-01-30 19:18:33 +0100

    Delete manager.d after compilation

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # clear components
-rm -r ~/.sel/components
+rm -r -f ~/.sel/components
 
 # download and compile manager
 wget https://raw.githubusercontent.com/sel-project/sel-manager/master/manager.d
 rdmd --build-only manager.d
+rm -r -f manager.d
 
 # move to /usr/bin/
 mv manager sel
